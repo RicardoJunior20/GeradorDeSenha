9f1e8aa744810ba14c6931832a4a0464f39ca456
Ricardo Junior <ricardo.marley18@gmail.com>
2024-05-25 10:15:05 -0300

    general modification to html, css and js

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
-let sliderElement = document.querySelector("#slider");
-let buttonElement = document.querySelector("#button");
+// Seleção de elementos do DOM
+const sliderElement = document.querySelector("#slider");
+const buttonElement = document.querySelector("#button");
+const sizePassword = document.querySelector("#valor");
+const password = document.querySelector("#password");
+const containerPassword = document.querySelector("#container-password");
 
-let sizePassword = document.querySelector("#valor");
-let password = document.querySelector("#password");
-
-let containerPassword = document.querySelector("#container-password");
-
-let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!!@#$%¨&*()_+{`}^>:<.;,~][´-=9';
+// Conjunto de caracteres para gerar a senha
+const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!!@#$%¨&*()_+{`}^>:<.;,~][´-=9';
 let novaSenha = '';
 
-sizePassword.innerHTML = sliderElement.value;
+// Atualiza o valor exibido do tamanho da senha conforme o slider é ajustado
+sizePassword.textContent = sliderElement.value;
+sliderElement.oninput = () => sizePassword.textContent = sliderElement.value;
 
-slider.oninput = function() {
-  sizePassword.innerHTML = this.value;
-}
-
-
-function generatePassword(){
-
+// Função para gerar a senha
+const generatePassword = () => {
   let pass = '';
-  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+  for (let i = 0, n = charset.length; i < sliderElement.value; i++) {
     pass += charset.charAt(Math.floor(Math.random() * n));
   }
-  
-  console.log(pass)
+
+  console.log(pass);
   containerPassword.classList.remove("hide");
-  password.innerHTML = pass;
+  password.textContent = pass;
   novaSenha = pass;
 }
 
-function copyPassword(){
-  alert("Senha copiada com sucesso!")
-  navigator.clipboard.writeText(novaSenha);
+// Função para copiar a senha para a área de transferência
+const copyPassword = async () => {
+  try {
+    await navigator.clipboard.writeText(novaSenha);
+    Swal.fire({
+      icon: 'success',
+      title: 'Sucesso!',
+      text: 'Senha copiada com sucesso!',
+      showConfirmButton: false,
+      timer: 1500
+    });
+  } catch (err) {
+    console.error("Erro ao copiar a senha: ", err);
+    Swal.fire({
+      icon: 'error',
+      title: 'Erro!',
+      text: 'Não foi possível copiar a senha.',
+    });
+  }
 }
+
+// Evento de clique para gerar a senha
+buttonElement.addEventListener("click", generatePassword);
+
+// Evento de clique para copiar a senha
+containerPassword.addEventListener("click", copyPassword);
